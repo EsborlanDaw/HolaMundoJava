1df6ca4c82e77c85c3c86a2e49a04fdb2a5c46c6
Estefania <esbrolan.daw@gmail.com>
2021-11-22 19:24:01 +0100

    He añadido hola mundo

---
+++ HolaMundoJava/src/holamundojava/HolaMundoJava.java
@@ -0,0 +1,23 @@
+/*
+ * Proyecto HolaMundoJava - Archivo HolaMundoJava.java - Compañia DAW
+ * Licencia Creative Commons BY-NC-SA 4.0
+ * https://creativecommons.org/licenses/by-nc-sa/4.0/
+ */
+package holamundojava;
+
+/**
+ *
+ * @author Estefania Boriko Laenos <esborlan.daw@gmail.com>
+ */
+public class HolaMundoJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("Hola Mundo");
+    }
+    
+}
